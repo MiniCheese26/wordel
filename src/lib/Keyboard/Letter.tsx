c43c89a25df54b3fb4841eddfd98e4f93a030c2a
Emily Medhurst <loc0ded@protonmail.com>
2022-07-30 13:20:42 +0100

    fix dumb

--- src/lib/Keyboard/Letter.tsx
+++ src/lib/Keyboard/Letter.tsx
@@ -1,5 +1,5 @@
 import {IconBackspace} from '@tabler/icons';
-import {useEffect, useRef} from 'react';
+import React, {useEffect} from 'react';
 
 type Props = {
   letter: string,
@@ -7,8 +7,6 @@ type Props = {
 }
 
 const Letter = ({ letter, onKeyDown }: Props) => {
-  const letterRef = useRef<HTMLDivElement | null>(null);
-
   let override;
 
   if (letter === '[') {
@@ -23,7 +21,8 @@ const Letter = ({ letter, onKeyDown }: Props) => {
     }
   };
 
-  const onClickHandler = () => {
+  const onClickHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
     let parsedLetter = letter;
 
     if (parsedLetter === '[') {
@@ -38,25 +37,17 @@ const Letter = ({ letter, onKeyDown }: Props) => {
   useEffect(() => {
     window.addEventListener('keydown', onKeyDownHandler);
 
-    if (letterRef.current) {
-      letterRef.current?.addEventListener('click', onClickHandler);
-    }
-
     return () => {
       window.removeEventListener('keydown', onKeyDownHandler);
-
-      if (letterRef.current) {
-        letterRef.current?.removeEventListener('click', onClickHandler);
-      }
     };
   });
 
   return (
-    <div ref={letterRef} className={`flex justify-center items-center font-semibold uppercase select-none ${override
+    <button onClick={onClickHandler} className={`flex justify-center items-center font-semibold uppercase select-none ${override
       ? 'flex-[2]'
       : 'flex-1'} text-sm md:text-md h-14 rounded-md bg-whiteHighlight hover:bg-whiteHighlightDim dark:bg-darkHighlight hover:dark:bg-darkHighlightDim hover:cursor-pointer`}>
       {override || letter}
-    </div>
+    </button>
   );
 };
 
