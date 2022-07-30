ae71dee997adc6e3611390f365403edceb3feb0a
Emily Medhurst <loc0ded@protonmail.com>
2022-07-30 20:57:09 +0100

    fix text colour

--- src/lib/Game/Letter.tsx
+++ src/lib/Game/Letter.tsx
@@ -16,44 +16,45 @@ const height = (LETTERS * 55) / LETTERS;
 
 const Letter = ({ letter, status }: Props) => {
   let backgroundColour: string;
-
-  switch (status) {
-    case LetterStatus.Exact:
-      backgroundColour = 'bg-green';
-      break;
-    case LetterStatus.InWord:
-      backgroundColour = 'bg-yellow';
-      break;
-    case LetterStatus.Invalid:
-      backgroundColour = 'bg-darker';
-      break;
-    default:
-      backgroundColour = 'transparent';
-      break;
-  }
-
   let borderColour: string;
 
   switch (status) {
     case LetterStatus.Exact:
+      backgroundColour = 'bg-green';
       borderColour = 'border-green';
       break;
     case LetterStatus.InWord:
+      backgroundColour = 'bg-yellow';
       borderColour = 'border-yellow';
       break;
     case LetterStatus.Invalid:
+      backgroundColour = 'bg-darker';
       borderColour = 'border-darker';
       break;
     default:
+      backgroundColour = 'transparent';
       borderColour = letter
         ? 'border-whiteHighlightBorder dark:border-darkHighlightBorder'
         : 'border-whiteHighlightActive dark:border-darkHighlightActive';
       break;
   }
 
+  let textColour: string;
+
+  switch (status) {
+    case LetterStatus.Invalid:
+    case LetterStatus.Exact:
+    case LetterStatus.InWord:
+      textColour = 'text-darkFg';
+      break;
+    default:
+      textColour = 'text-whiteFg dark:text-darkFg';
+      break;
+  }
+
   return (
     <div
-      className={`flex flex-1 justify-center items-center font-bold text-2xl uppercase select-none border-2 text-darkFg ${borderColour} ${backgroundColour}`}
+      className={`flex flex-1 justify-center items-center font-bold text-2xl uppercase select-none border-2 ${textColour} ${borderColour} ${backgroundColour}`}
       style={{ height: `${height}px` }}>
       {letter}
     </div>
